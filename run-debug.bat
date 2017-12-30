22923d540d70ffd47fab124041e2771f2654e34c
jottocraft <jack@loefflerfamily.com>
2017-12-30 11:58:26 -0800

    Debug menu and other stuff

--- run-debug.bat
+++ run-debug.bat
@@ -1,27 +1,5 @@
 @echo off
-:init
-set "started="
-2>nul (
- 9>"%~f0.lock" (
-  set "started=1"
-  call :start
- )
-)
-@if defined started (
-    del "%~f0.lock" >nul 2>nul
-) else (
-    echo Process aborted: "%~f0" is already running
-    @ping localhost > nul
-)
-
-exit /b
-
-
-
-
-:start
-cd /d %~dp0
-echo Google Assistant is now running
+echo Starting the Google Assistant in debug mode...
 googlesamples-assistant-pushtotalk --device-model-id jottocraft1 --project-id premium-episode-182223
 echo An error has occured. Please open the menu in the Google Assistant app, and run the service in debug mode. Then screenshot the issue and add it to GitHub so it can be fixed.
 pause
