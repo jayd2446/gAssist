43c15ccb66f8e90bc4895037eea4270aff3d33e3
jottocraft <jack@loefflerfamily.com>
2017-12-30 10:59:04 -0800

    run should now be more reliable

--- run.bat
+++ run.bat
@@ -4,4 +4,4 @@ set minimized=true
 start /min cmd /C "%~dpnx0"
 goto :EOF
 :minimized
-%LocalAppData%\Programs\Python\Python36-32\Scripts\googlesamples-assistant-pushtotalk.exe --device-model-id jottocraft1 --project-id premium-episode-182223
+googlesamples-assistant-pushtotalk --device-model-id jottocraft1 --project-id premium-episode-182223
